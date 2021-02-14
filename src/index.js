5f9f1a1f1213dec84b9d83585e3ffb4f710b552f
Alexander <alexanchek@sashkin-blog.ru>
2021-02-14 09:37:07 +0300

    fix: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    strNumber = String(n);
+    arrNumber = strNumber.split("");
+
+    if (strNumber[0] != "-") {
+        return arrNumber.reverse().join("");
+    } else {
+        strNegNumber = "";
+        for (let i = arrNumber.length - 1; i > 0; i--) {
+            strNegNumber += arrNumber[i];
+        }
+        return strNegNumber;
+    }
+};
